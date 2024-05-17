7889163ed5d0838518a951085dcfbc73789d6a8b
ilhan <ilhanabdalle@gmail.com>
2024-05-17 14:28:12 +0100

    adding nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import { connectToDatabase } from "./src/config/db_connection";
+import { connectToDatabase } from "./config/db_connection";
 import express, { Application } from 'express';
 import dotenv from 'dotenv'; 
 require('dotenv').config();
 
 const app: Application = express();
+
 
 const listenForRequests = (): void => {
     const port : string | undefined = process.env.PORT;
@@ -11,7 +12,6 @@ const listenForRequests = (): void => {
     if (!port) {
         throw new Error('PORT environment variable is not set');
     }
-
     app.listen(parseInt(port), () => {
         console.log("Now listening on port", port);
     });
