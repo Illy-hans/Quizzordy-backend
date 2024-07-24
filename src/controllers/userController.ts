219a21e41592cccf2edb4007642a277710b540fb
ilhan <ilhanabdalle@gmail.com>
2024-07-24 15:33:31 +0100

    controller refactor - create + login in authentication

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,55 +2,6 @@ import { User, IUser } from "../models/userModel";
 import { generateToken } from "../middleware/token";
 import { Request, Response } from 'express';
 import { QuizEntry } from "../models/quizModel";
-import bcrypt from 'bcrypt';
-
-const create = async (req: Request, res: Response): Promise<Response> => {
-    const { username, email, password } = req.body;
-
-    try {
-        const existingUser: IUser | null = await User.findOne({ email });
-    
-        if (existingUser) {
-            return res.status(401).json({ message: 'Email already in use' });
-        };
-
-        const newUser = new User({ username, email, password });
-
-        await newUser.save();
-        console.log("User created, id:", newUser._id.toString());
-        const token = generateToken(newUser.id);
-        return res.status(201).json({ message: "User created", token});
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({ message: "Something went wrong" });
-        };
-    
-};
-
-const authenticate = async (req: Request, res: Response): Promise<Response> => {
-    const { email, password } = req.body;
-
-    try {
-        const existingUser = await User.findOne({ email: email });
-
-        if (!existingUser) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Invalid credentials" });
-        }
-
-        const token = generateToken(existingUser.id);
-        return res.status(200).json({ message: "Authentication successful", token: token, username: existingUser.username, user_id: existingUser._id});
-
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({ message: "Authentication failed" });
-    }
-};
 
 
 interface CustomRequest extends Request {
@@ -135,8 +86,6 @@ const saveQuiz = async( req: CustomRequest, res: Response): Promise<Response> =>
 
 
 const UsersController = {
-    create: create,
-    authenticate: authenticate,
     getAllUserData: getAllUserData, 
     updateUserData: updateUserData,
     saveQuiz: saveQuiz,
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { tokenChecker } from "../middleware/token_checker";
 import { UsersController } from "../controllers/userController";
+import { AuthenticationController } from "src/controllers/authenticationController";
 
 const router: Router = Router();
 
-router.post("/", UsersController.create);
-router.post("/authenticate", UsersController.authenticate)
+router.post("/", AuthenticationController.create);
+router.post("/authenticate", AuthenticationController.authenticate)
 router.get("/:id", tokenChecker, UsersController.getAllUserData);
 router.patch("/:id",tokenChecker, UsersController.updateUserData);
 router.patch("/:id/addquiz", tokenChecker, UsersController.saveQuiz);
