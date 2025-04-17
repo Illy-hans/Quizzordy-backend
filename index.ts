d13b0a3c8386db63ee292d724818734bc20aff91
ilhan <ilhanabdalle@gmail.com>
2025-04-17 12:15:28 +0100

    Vercel update

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import serverless from 'serverless-http';
-import app from '../src/app'; // or './app' depending on your structure
+import app from './src/app'; // or './app' depending on your structure
 
 const handler = serverless(app);
 
